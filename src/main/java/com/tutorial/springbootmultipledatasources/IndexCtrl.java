af3217b1af56f954834e07c10b0fcf4a815ada98
john <john.jiang@bestwehotel.com>
2018-11-15 18:43:54 +0800

    #init

--- src/main/java/com/tutorial/springbootmultipledatasources/IndexCtrl.java
+++ src/main/java/com/tutorial/springbootmultipledatasources/IndexCtrl.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalTime;
+
 /**
  * Created by jiangzs@gmail.com on 2018/10/29.
  */
@@ -13,6 +15,6 @@ public class IndexCtrl {
 
     @RequestMapping("/")
     public String index() {
-        return "helloWorld";
+        return LocalTime.now().toString();
     }
 }
